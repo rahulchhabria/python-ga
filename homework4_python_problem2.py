d00f3d000e0be597433098fe65f987f5509f652d
Rahul Chhabria <rahul.chhabria@gmail.com>
2020-01-01 21:45:16 -0800

    homework

---
+++ homework4_python_problem2.py
@@ -0,0 +1,42 @@
+employee1 = {
+    "name": "Ron Swanson", 
+    "age": 55,
+    "department": "Management", 
+    "phone": "555-1234", 
+    "salary": ",000"
+    
+}
+
+employee2 = {
+    
+    "name": "Leslie Knope", 
+    "age": 55,
+    "department": "Middle Management", 
+    "phone": "555-4321", 
+    "salary": ",000"
+    
+}
+    
+employee3 = {
+    "name": "Andy Dwyer", 
+    "age": 55,
+    "department": "Shoe Shining", 
+    "phone": "555-1122", 
+    "salary": ",000"
+    
+}
+
+employee4 = {
+    
+    "name": "April Ludgate", 
+    "age": 55,
+    "department": "Administration", 
+    "phone": "555-3345", 
+    "salary": ",000"
+    
+}
+
+print(employee1["name"], "in", employee1["department"], "can be reached at", employee1["phone"], ".")
+print(employee2["name"], "in", employee2["department"], "can be reached at", employee2["phone"], ".")
+print(employee3["name"], "in", employee3["department"], "can be reached at", employee3["phone"], ".")
+print(employee4["name"], "in", employee4["department"], "can be reached at", employee4["phone"], ".")
